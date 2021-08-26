d0941bc9f631af25a673cd7636675d6eb9dd15a5
portasynthinca3 <portasynthinca3@gmail.com>
2021-08-27 00:39:16 +0300

    add io bus

--- include/machine.h
+++ include/machine.h
@@ -5,6 +5,7 @@
 // Definitions
 
 #define TRACE_MEM_ACCESS
+#define TRACE_IO_ACCESS
 
 #define BIOS_START 0xfe000
 #define RAM_SIZE   64 * 1024
@@ -14,16 +15,34 @@
 
 // Private functions
 
-void    _machine_mem_wr(uint32_t addr, uint8_t val);
-uint8_t _machine_mem_rd(uint32_t addr);
+void     _machine_mem_wr (uint32_t addr, uint8_t val);
+uint8_t  _machine_mem_rd (uint32_t addr);
+
+void     _machine_io_wr8 (uint16_t port, uint8_t val);
+void     _machine_io_wr16(uint16_t port, uint16_t val);
+uint8_t  _machine_io_rd8 (uint16_t port);
+uint16_t _machine_io_rd16(uint16_t port);
 
 // Public functions
 
 void    machine_init  (void);
+
 #ifdef TRACE_MEM_ACCESS
 void    machine_mem_wr(uint32_t addr, uint8_t val);
 uint8_t machine_mem_rd(uint32_t addr);
 #else
 #define machine_mem_wr _machine_mem_wr
 #define machine_mem_rd _machine_mem_rd
+#endif
+
+#ifdef TRACE_IO_ACCESS
+void     machine_io_wr8 (uint16_t port, uint8_t val);
+void     machine_io_wr16(uint16_t port, uint16_t val);
+uint8_t  machine_io_rd8 (uint16_t port);
+uint16_t machine_io_rd16(uint16_t port);
+#else
+#define machine_io_wr8  _machine_io_wr8
+#define machine_io_wr16 _machine_io_wr16
+#define machine_io_rd8  _machine_io_rd8
+#define machine_io_rd16 _machine_io_rd16
 #endif
--- src/components/machine.c
+++ src/components/machine.c
@@ -12,13 +12,7 @@ uint8_t* main_ram = NULL;
 extern const uint8_t bios_data_start[] asm("_binary_bios_bin_start");
 extern const uint8_t bios_data_end[] asm("_binary_bios_bin_end");
 
-// Public functions
-
-void machine_init(void) {
-    // allocate main RAM
-    main_ram = realloc(main_ram, RAM_SIZE);
-    memset(main_ram, 0, RAM_SIZE);
-}
+// Private functions
 
 uint8_t _machine_mem_rd(uint32_t addr) {
     if(addr < RAM_SIZE)
@@ -41,15 +35,57 @@ void _machine_mem_wr(uint32_t addr, uint8_t val) {
     return;
 }
 
+void _machine_io_wr8(uint16_t port, uint8_t val) {
+    // TODO
+}
+void _machine_io_wr16(uint16_t port, uint16_t val) {
+    // TODO
+}
+uint8_t _machine_io_rd8(uint16_t port) {
+    // TODO
+}
+uint16_t _machine_io_rd16(uint16_t port) {
+    // TODO
+}
+
+// Public functions
+
+void machine_init(void) {
+    // allocate main RAM
+    main_ram = realloc(main_ram, RAM_SIZE);
+    memset(main_ram, 0, RAM_SIZE);
+}
+
 #ifdef TRACE_MEM_ACCESS
 uint8_t machine_mem_rd(uint32_t addr) {
     uint8_t val = _machine_mem_rd(addr);
-    ESP_LOGI(TAG, "trace: RD 0x%05x: %02x", addr, val);
+    ESP_LOGI(TAG, "trace-mem: RD 0x%05x: %02x", addr, val);
     return val;
 }
 
 void machine_mem_wr(uint32_t addr, uint8_t val) {
-    ESP_LOGI(TAG, "trace: WR 0x%05x: %02x", addr, val);
+    ESP_LOGI(TAG, "trace-mem: WR 0x%05x: %02x", addr, val);
     _machine_mem_wr(addr, val);
+}
+#endif
+
+#ifdef TRACE_IO_ACCESS
+void machine_io_wr8(uint16_t port, uint8_t val) {
+    ESP_LOGI(TAG, "trace-io: WR8 0x%04x: %02x", port, val);
+    _machine_io_wr8(port, val);
+}
+void machine_io_wr16(uint16_t port, uint16_t val) {
+    ESP_LOGI(TAG, "trace-io: WR16 0x%04x: %04x", port, val);
+    _machine_io_wr16(port, val);
+}
+uint8_t machine_io_rd8(uint16_t port) {
+    uint8_t val = _machine_io_rd8(port);
+    ESP_LOGI(TAG, "trace-io: RD8 0x%04x: %02x", port, val);
+    return val;
+}
+uint16_t machine_io_rd16(uint16_t port) {
+    uint16_t val = _machine_io_rd8(port);
+    ESP_LOGI(TAG, "trace-io: RD16 0x%04x: %04x", port, val);
+    return val;
 }
 #endif
